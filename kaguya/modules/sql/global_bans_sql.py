7bbe26970615b16726fdecd5bb50bc922fac1773
minatouzuki <72920726+minatouzuki@users.noreply.github.com>
2021-01-03 23:26:55 +0530

    Update global_bans_sql.py

--- kaguya/modules/sql/global_bans_sql.py
+++ kaguya/modules/sql/global_bans_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, UnicodeText, Integer, String, Boolean
 
-from perry.modules.sql import BASE, SESSION
+from kaguya.modules.sql import BASE, SESSION
 
 
 class GloballyBannedUsers(BASE):
