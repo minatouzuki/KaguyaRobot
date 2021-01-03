4a69da2c4b497d62388f232225c637ace8f55aae
minatouzuki <72920726+minatouzuki@users.noreply.github.com>
2021-01-03 23:19:17 +0530

    Update msg_types.py

--- kaguya/modules/helper_funcs/msg_types.py
+++ kaguya/modules/helper_funcs/msg_types.py
@@ -2,7 +2,7 @@ from enum import IntEnum, unique
 
 from telegram import Message
 
-from perry.modules.helper_funcs.string_handling import button_markdown_parser
+from kaguya.modules.helper_funcs.string_handling import button_markdown_parser
 
 
 @unique
