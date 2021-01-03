b9c79c8f39bf517b5c8b5df0145814b1832e352e
minatouzuki <72920726+minatouzuki@users.noreply.github.com>
2021-01-03 23:27:59 +0530

    Update log_channel_sql.py

--- kaguya/modules/sql/log_channel_sql.py
+++ kaguya/modules/sql/log_channel_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, func, distinct
 
-from perry.modules.sql import BASE, SESSION
+from kaguya.modules.sql import BASE, SESSION
 
 
 class GroupLogs(BASE):
