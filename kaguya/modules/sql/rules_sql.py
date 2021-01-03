28fb1bfaa4ddbbddf3a2408813857aafe3a881f9
minatouzuki <72920726+minatouzuki@users.noreply.github.com>
2021-01-03 23:29:44 +0530

    Update rules_sql.py

--- kaguya/modules/sql/rules_sql.py
+++ kaguya/modules/sql/rules_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, func, distinct
 
-from perry.modules.sql import SESSION, BASE
+from kaguya.modules.sql import SESSION, BASE
 
 
 class Rules(BASE):
