56094d82d4fb4ee1a60076aa5d14220a09084ff4
minatouzuki <72920726+minatouzuki@users.noreply.github.com>
2021-01-03 23:18:47 +0530

    Update misc.py

--- kaguya/modules/helper_funcs/misc.py
+++ kaguya/modules/helper_funcs/misc.py
@@ -3,7 +3,7 @@ from typing import List, Dict
 from telegram import MAX_MESSAGE_LENGTH, InlineKeyboardButton, Bot, ParseMode
 from telegram.error import TelegramError
 
-from perry import LOAD, NO_LOAD
+from kaguya import LOAD, NO_LOAD
 
 
 class EqInlineKeyboardButton(InlineKeyboardButton):
