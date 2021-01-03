da1c2a7fbaeab75088ef5added3e6e221e3ef642
minatouzuki <72920726+minatouzuki@users.noreply.github.com>
2021-01-03 23:17:01 +0530

    Update extraction.py

--- kaguya/modules/helper_funcs/extraction.py
+++ kaguya/modules/helper_funcs/extraction.py
@@ -3,8 +3,8 @@ from typing import List, Optional
 from telegram import Message, MessageEntity
 from telegram.error import BadRequest
 
-from perry import LOGGER
-from perry.modules.users import get_user_id
+from kaguya import LOGGER
+from kaguya.modules.users import get_user_id
 
 
 def id_from_reply(message):
