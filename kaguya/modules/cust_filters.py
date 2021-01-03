d4f434868fa3c0ca04da26f61b6086ca93eba087
minatouzuki <72920726+minatouzuki@users.noreply.github.com>
2021-01-03 22:45:20 +0530

    Update cust_filters.py

--- kaguya/modules/cust_filters.py
+++ kaguya/modules/cust_filters.py
@@ -12,24 +12,24 @@ from telegram.ext import (
 )
 from telegram.utils.helpers import mention_html, escape_markdown
 
-from perry import dispatcher, LOGGER
-from perry.modules.disable import DisableAbleCommandHandler
-from perry.modules.helper_funcs.chat_status import user_admin
-from perry.modules.helper_funcs.extraction import extract_text
-from perry.modules.helper_funcs.filters import CustomFilters
-from perry.modules.helper_funcs.misc import build_keyboard_parser
-from perry.modules.helper_funcs.msg_types import get_filter_type
-from perry.modules.helper_funcs.string_handling import (
+from kaguya import dispatcher, LOGGER
+from kaguya.modules.disable import DisableAbleCommandHandler
+from kaguya.modules.helper_funcs.chat_status import user_admin
+from kaguya.modules.helper_funcs.extraction import extract_text
+from kaguya.modules.helper_funcs.filters import CustomFilters
+from kaguya.modules.helper_funcs.misc import build_keyboard_parser
+from kaguya.modules.helper_funcs.msg_types import get_filter_type
+from kaguya.modules.helper_funcs.string_handling import (
     split_quotes,
     button_markdown_parser,
     escape_invalid_curly_brackets,
     markdown_to_html,
 )
-from perry.modules.sql import cust_filters_sql as sql
+from kaguya.modules.sql import cust_filters_sql as sql
 
-from perry.modules.connection import connected
+from kaguya.modules.connection import connected
 
-from perry.modules.helper_funcs.alternate import send_message, typing_action
+from kaguya.modules.helper_funcs.alternate import send_message, typing_action
 
 HANDLER_GROUP = 15
 
