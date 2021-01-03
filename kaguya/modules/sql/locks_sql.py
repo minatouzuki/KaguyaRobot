992714375c2e9fd0a53248db87e9ee2c0a1579e7
minatouzuki <72920726+minatouzuki@users.noreply.github.com>
2021-01-03 23:27:21 +0530

    Update locks_sql.py

--- kaguya/modules/sql/locks_sql.py
+++ kaguya/modules/sql/locks_sql.py
@@ -3,7 +3,7 @@ import threading
 
 from sqlalchemy import Column, String, Boolean
 
-from perry.modules.sql import SESSION, BASE
+from kaguya.modules.sql import SESSION, BASE
 
 
 class Permissions(BASE):
